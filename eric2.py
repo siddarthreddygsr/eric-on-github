4061729674ad12ba80d9440e52c23b1e482140ae
Siddarth Reddy Gurram <u20144@INHYDMECSC01.mechyd.ad>
2022-01-01 09:58:50 +0530

    added code to compose

--- eric2.py
+++ eric2.py
@@ -1,4 +1,3 @@
-#v2.2
 import glob
 import pickle
 import numpy
@@ -11,25 +10,21 @@ from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import BatchNormalization as BatchNorm
 from tensorflow.keras.callbacks import ModelCheckpoint
-import multiprocessing
 import concurrent.futures
 
 notes = []
-processes = []
-
-def parse(file):
-
+def parse(mid):
 	global notes
-	midi = converter.parse(file)
+	midi = converter.parse(mid)
 
-	print("Parsing %s" % file)
+	print("Parsing %s" % mid)
 
 	notes_to_parse = None
 
-	try: # file has instrument parts
+	try:
 		s2 = instrument.partitionByInstrument(midi)
 		notes_to_parse = s2.parts[0].recurse()
-	except: # file has notes in a flat structure
+	except:
 		notes_to_parse = midi.flat.notes
 
 	for element in notes_to_parse:
@@ -38,41 +33,27 @@ def parse(file):
 		elif isinstance(element, chord.Chord):
 			notes.append('.'.join(str(n) for n in element.normalOrder))
 
-
-
 def main():
-
+	
 	global notes
-	# for file in glob.glob("archive/Jazz-mid/*.mid"):
-	# 	process = multiprocessing.Process(target=parse, args=[file])
-	# 	process.start()
-	# 	processes.append(process)
-	print(multiprocessing.cpu_count())
-	with concurrent.futures.ThreadPoolExecutor(multiprocessing.cpu_count()) as executor:
+
+	with concurrent.futures.ThreadPoolExecutor() as executor:
 		results = [executor.map(parse, glob.glob("archive/Jazz-mid/*.mid"))]
 
-	# for process in processes:
-	# 	process.join()
-
-	print("notes:",notes)
-
-	with open('notes', 'wb') as filepath:
+	with open('pickle/notes', 'wb') as filepath:
 		pickle.dump(notes, filepath)
-	# get amount of pitch names
+
 	n_vocab = len(set(notes))
-	""" Prepare the sequences used by the Neural Network """
+
 	sequence_length = 100
 
-	# get all pitch names
 	pitchnames = sorted(set(item for item in notes))
 
-	 # create a dictionary to map pitches to integers
 	note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
 
 	network_input = []
 	network_output = []
 
-	# create input sequences and the corresponding outputs
 	for i in range(0, len(notes) - sequence_length, 1):
 		sequence_in = notes[i:i + sequence_length]
 		sequence_out = notes[i + sequence_length]
@@ -81,19 +62,13 @@ def main():
 
 	n_patterns = len(network_input)
 
-	# reshape the input into a format compatible with LSTM layers
 	network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
-	# normalize input
+
 	network_input = network_input / float(n_vocab)
 
 	network_output = tensorflow.keras.utils.to_categorical(network_output)
 
-	""" create the structure of the neural network """
-
-	#creating an instance of the GPU
-	#specifically uses the 8 GPUs of the NVIDIA DGX-1
-	mirrored_strategy = tensorflow.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1","/gpu:2","/gpu:3","/gpu:4","/gpu:5","/gpu:6","/gpu:7"])
-
+	mirrored_strategy = tensorflow.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1","/gpu:2","/gpu:3"])
 	with mirrored_strategy.scope():
 		model = Sequential()
 		model.add(LSTM(
@@ -112,11 +87,9 @@ def main():
 		model.add(Dropout(0.3))
 		model.add(Dense(n_vocab))
 		model.add(Activation('softmax'))
+		model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-	model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-
-	# train the network
-	filepath = "epoch-{epoch:02d}-loss-{loss:.4f}-model.hdf5"
+	filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
 	checkpoint = ModelCheckpoint(
 		filepath,
 		monitor='loss',
@@ -128,6 +101,5 @@ def main():
 
 	model.fit(network_input, network_output, epochs=200, batch_size=128, callbacks=callbacks_list)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
---
+++ ericCompose.py
@@ -0,0 +1,110 @@
+import pickle
+import numpy
+from music21 import instrument, note, stream, chord
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import BatchNormalization as BatchNorm
+from tensorflow.keras.layers import Activation
+import tensorflow
+
+def make_model(network_input, n_vocab):
+
+	model = Sequential()
+	model.add(LSTM(
+		512,
+		input_shape=(network_input.shape[1], network_input.shape[2]),
+		recurrent_dropout=0.3,
+		return_sequences=True
+	))
+	model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3,))
+	model.add(LSTM(512))
+	model.add(BatchNorm())
+	model.add(Dropout(0.3))
+	model.add(Dense(256))
+	model.add(Activation('relu'))
+	model.add(BatchNorm())
+	model.add(Dropout(0.3))
+	model.add(Dense(n_vocab))
+	model.add(Activation('softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+	model.load_weights('weights-improvement-01-5.1199-bigger.hdf5')
+
+	return model
+
+
+def main():
+	with open('data/notes', 'rb') as filepath:
+		notes = pickle.load(filepath)
+	pitchnames = sorted(set(item for item in notes))
+	n_vocab = len(set(notes))
+	note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+
+	sequence_length = 100
+	network_input = []
+	output = []
+	for i in range(0, len(notes) - sequence_length, 1):
+		sequence_in = notes[i:i + sequence_length]
+		sequence_out = notes[i + sequence_length]
+		network_input.append([note_to_int[char] for char in sequence_in])
+		output.append(note_to_int[sequence_out])
+
+	n_patterns = len(network_input)
+
+	normalized_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
+
+	normalized_input = normalized_input / float(n_vocab)
+
+
+
+	model = make_model(normalized_input, n_vocab)
+	start = numpy.random.randint(0, len(network_input)-1)
+
+	int_to_note = dict((number, note) for number, note in enumerate(pitchnames))
+
+	pattern = network_input[start]
+	prediction_output = []
+
+	for note_index in range(500):
+		prediction_input = numpy.reshape(pattern, (1, len(pattern), 1))
+		prediction_input = prediction_input / float(n_vocab)
+
+		prediction = model.predict(prediction_input, verbose=0)
+
+		index = numpy.argmax(prediction)
+		result = int_to_note[index]
+		prediction_output.append(result)
+
+		pattern.append(index)
+		pattern = pattern[1:len(pattern)]
+	
+	offset = 0
+	output_notes = []
+
+	for pattern in prediction_output:
+		if ('.' in pattern) or pattern.isdigit():
+			notes_in_chord = pattern.split('.')
+			notes = []
+			for current_note in notes_in_chord:
+				new_note = note.Note(int(current_note))
+				new_note.storedInstrument = instrument.Piano()
+				notes.append(new_note)
+			new_chord = chord.Chord(notes)
+			new_chord.offset = offset
+			output_notes.append(new_chord)
+		else:
+			new_note = note.Note(pattern)
+			new_note.offset = offset
+			new_note.storedInstrument = instrument.Piano()
+			output_notes.append(new_note)
+
+		offset += 0.5
+
+	midi_stream = stream.Stream(output_notes)
+
+	midi_stream.write('midi', fp='test_output-nofunc.mid')
+
+if __name__=="__main__":
+	main()
